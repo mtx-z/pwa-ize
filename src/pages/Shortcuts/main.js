8474a0567f3320d8cd7665cbbcc83cd76088d149
Mtxz <matix@hotmail.fr>
2022-02-28 19:27:05 +0100

    clean

--- src/pages/Shortcuts/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue"
-import App from "./App.vue"
-
-import "materialize-css/dist/css/materialize.min.css"
-import "material-design-icons/iconfont/material-icons.css"
-import "@/css/style.css"
-
-createApp(App).mount("#app")
